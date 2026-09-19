adf2cbe5b393933ca69ca301851552e96f36b675
Adam Egbert <adamegbert@icloud.com>
2026-09-19 00:00:05 -0600

    finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+
+
+        Console.Write("What is your grade? \n >>> ");
+            int numgrade = int.Parse(Console.ReadLine());
+
+            if (numgrade >= 90)
+                Console.Write("Congratulations! You got an A!");
+            else if (numgrade >= 80)
+                Console.Write("Congradulations! You got a B!");
+            else if (numgrade >= 70)
+                Console.Write("Congradulations! You got a C!");
+            else if (numgrade >= 60)
+                Console.Write("Better luck next time. You got a D.");
+            else
+                Console.Write("You should have tried harder. You didn't pass.");
+
+            // Console.WriteLine(grade);
     }
 }
