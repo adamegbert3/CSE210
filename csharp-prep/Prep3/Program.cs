31e14492d386b87479f182c91b11d0700051395e
Adam Egbert <adamegbert@icloud.com>
2026-09-24 10:18:07 -0600

    added the computer generated numbers. Prep 3 done.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,21 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number of your choice? ");
-        int userMagicNumber = int.Parse(Console.ReadLine());
+        // Console.Write("What is the magic number of your choice? ");
+        // int userMagicNumber = int.Parse(Console.ReadLine());
+
+
+        Random randomNumberGenerator = new Random();
+        int computerMagicNumber = randomNumberGenerator.Next(1, 100);
+
 
         int userGuess = 0;
 
-        while (userGuess != userMagicNumber)
+        // while (userGuess != userMagicNumber)
+        // {
+        //     Console.Write("What is your guess for the Magic Number? ");
+        //     userGuess = int.Parse(Console.ReadLine());
+
+        //     if (userMagicNumber > userGuess)
+        //     {
+        //         Console.Write("Higher");
+        //     }
+        //     else if (userMagicNumber < userGuess)
+        //     {
+        //         Console.Write("Lower");
+        //     }
+        //     else
+        //     {
+        //         Console.Write("CORRECT! YOU GUESSED IT :)");
+        //     }
+        // }
+
+        while (userGuess != computerMagicNumber)
         {
             Console.Write("What is your guess for the Magic Number? ");
             userGuess = int.Parse(Console.ReadLine());
 
-            if (userMagicNumber > userGuess)
+            if (computerMagicNumber > userGuess)
             {
                 Console.Write("Higher");
             }
-            else if (userMagicNumber < userGuess)
+            else if (computerMagicNumber < userGuess)
             {
                 Console.Write("Lower");
             }
