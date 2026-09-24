2dcf1d29bbc7b172240dca99039f96ded9331fb0
Adam Egbert <adamegbert@icloud.com>
2026-09-24 11:06:59 -0600

    Prep 4: input numbers and sum works

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Please enter numbers. Type 0 when done. ");
+
+            string userintegers = Console.ReadLine();
+            userNumber = int.Parse(userintegers);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.Write($"The sum is {sum}");
     }
 }
